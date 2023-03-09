12351943e8c34971a9120a2b1d9111a60f7820ec
Tadas Nevinskas <tadasnevinskas@gmail.com>
2023-03-09 22:15:43 +0200

    Add more baud rates, change default baud, minor improvement

--- com.c
+++ com.c
@@ -45,11 +45,50 @@
 #include <sys/ioctl.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <stdint.h>
 
 int transfer_byte(int from, int to, int is_control);
 
-typedef struct {char *name; int flag; } speed_spec;
+typedef struct {
+	char *name;
+	int flag;
+} speed_spec;
 
+const speed_spec speeds[] =
+{
+	{"0", B0},
+	{"50", B50},
+	{"75", B75},
+	{"110", B110},
+	{"134", B134},
+	{"150", B150},
+	{"200", B200},
+	{"300", B300},
+	{"600", B600},
+	{"1200", B1200},
+	{"2400", B2400},
+	{"4800", B4800},
+	{"9600", B9600},
+	{"19200", B19200},
+	{"38400", B38400},
+	{"57600", B57600},
+	{"115200", B115200},
+	{"115200", B115200},
+	{"230400", B230400},
+	{"460800", B460800},
+	{"500000", B500000},
+	{"576000", B576000},
+	{"921600", B921600},
+	{"1000000", B1000000},
+	{"1152000", B1152000},
+	{"1500000", B1500000},
+	{"2000000", B2000000},
+	{"2500000", B2500000},
+	{"3000000", B3000000},
+	{"3500000", B3500000},
+	{"4000000", B4000000},
+	{NULL, 0}
+};
 
 void print_status(int fd) {
 	int status;
@@ -65,6 +104,18 @@ void print_status(int fd) {
 	fprintf(stderr, "\r\n");
 }
 
+static void usage(char *name)
+{
+	uint8_t i = 0;
+
+	fprintf(stderr, "example: %s /dev/ttyS0 [115200]\n\n", name);
+	fprintf(stderr, "available baud rates:\n");
+
+	while (speeds[i].name)
+		fprintf(stderr, "\t %s \n", speeds[i++].name);
+
+}
+
 int main(int argc, char *argv[])
 {
 	int comfd;
@@ -72,22 +123,10 @@ int main(int argc, char *argv[])
 	struct termios oldkey, newkey;       //place tor old and new port settings for keyboard teletype
 	char *devicename = argv[1];
 	int need_exit = 0;
-	speed_spec speeds[] =
-	{
-		{"1200", B1200},
-		{"2400", B2400},
-		{"4800", B4800},
-		{"9600", B9600},
-		{"19200", B19200},
-		{"38400", B38400},
-		{"57600", B57600},
-		{"115200", B115200},
-		{NULL, 0}
-	};
-	int speed = B9600;
+	int speed = B115200;
 
 	if(argc < 2) {
-		fprintf(stderr, "example: %s /dev/ttyS0 [115200]\n", argv[0]);
+		usage(argv[0]);
 		exit(1);
 	}
 
@@ -98,14 +137,15 @@ int main(int argc, char *argv[])
 		exit(-1);
 	}
 
-	if(argc > 2) {	
-		speed_spec *s;
+	if(argc > 2) {
+		const speed_spec *s;
 		for(s = speeds; s->name; s++) {
-			if(strcmp(s->name, argv[2]) == 0) {
-				speed = s->flag;
-				fprintf(stderr, "setting speed %s\n", s->name);
-				break;
-			}
+			if(strcmp(s->name, argv[2]) != 0)
+				continue;
+
+			speed = s->flag;
+			fprintf(stderr, "setting speed %s\n", s->name);
+			break;
 		}
 	}
 
@@ -137,7 +177,7 @@ int main(int argc, char *argv[])
 	while(!need_exit) {
 		fd_set fds;
 		int ret;
-		
+
 		FD_ZERO(&fds);
 		FD_SET(STDIN_FILENO, &fds);
 		FD_SET(comfd, &fds);
